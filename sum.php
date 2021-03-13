7d7fe69a04c705af9965690ae6d799aac5f9c8db
miki saki <ss10863008@gmail.com>
2021-03-13 13:17:58 +0000

    10th commit

--- sum.php
+++ sum.php
@@ -87,13 +87,20 @@ echo "\n";
 // 課題4
 // 配列の中で一番大きい値を返す max_array という関数を実装
 // 途中の部分を完成させる
+echo "-------課題４-------";
+echo "\n";
+
 function max_array($arr) {
     $max_number = $arr[0];
     foreach($arr as $a) {
-   
+        if($a > $max_number) {
+            $max_number = $a; 
+        }
     }
     return $max_number;
 }
+echo max_array(array(1,5,3,8,9,2));
+
 
 echo "\n";
 
